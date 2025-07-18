9fd50be8b19d0b779079bd2f16056d68acb487df
Abdelhadi RANEM <abdelhadi@MacBook-Pro-de-Abdelhadi.local>
2025-07-18 23:09:34 +0200

    ADD : removed the sexe section for mails now there is only one mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { Copy } from 'lucide-react';
 
 function Footer({ onCopy }) {
   const emails = [
-    { label: "🔵", email: "lesbasesdelislam00@gmail.com" },
-    { label: "🟣", email: "lesbasesdelislamfemme@gmail.com" },
+    { email: "lesbasesdelislam00@gmail.com" },
+    // { label: "🟣", email: "lesbasesdelislamfemme@gmail.com" },
   ];
 
   const handleCopy = (email) => {
@@ -15,13 +15,13 @@ function Footer({ onCopy }) {
     <footer className="w-full px-4 text-center text-sm text-gray-200 dark:text-gray-200 py-6">
       <div className="w-full sm:w-[80%] md:w-[60%] flex flex-col items-center gap-2 p-3 rounded-xl bg-black/30 backdrop-blur-md shadow-md transition-all duration-300 hover:shadow-lg hover:scale-[1.01] mx-auto">
         <p className="mb-2 font-medium">Une remarque, un bug ou une idée d'amélioration ?</p>
-        {emails.map(({ label, email }, i) => (
+        {emails.map(({ email }, i) => (
           <div
             key={i}
             className="flex items-center gap-2 cursor-pointer group"
             onClick={() => handleCopy(email)}
           >
-            <span className="text-gray-800 dark:text-white font-semibold">{label}:</span>
+            {/* <span className="text-gray-800 dark:text-white font-semibold">{label}:</span> */}
             <span className="text-green-600 dark:text-green-400 font-medium underline">{email}</span>
             <Copy size={16} className="text-gray-600 dark:text-gray-300 group-hover:text-green-500 transition-colors" />
           </div>
